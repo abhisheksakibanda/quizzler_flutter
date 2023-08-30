a862c8592440f7e273649dfe239927ae5ccaef06
Abhishek Sakibanda <abhisheksakibanda@gmail.com>
2023-08-30 19:25:42 -0400

    simplified constructor

--- lib/question.dart
+++ lib/question.dart
@@ -2,10 +2,7 @@ class Question {
   String _questionText = '';
   bool _questionAnswer = false;
 
-  Question(String q, bool a) {
-    _questionText = q;
-    _questionAnswer = a;
-  }
+  Question(this._questionText, this._questionAnswer);
 
   bool get questionAnswer => _questionAnswer;
 
