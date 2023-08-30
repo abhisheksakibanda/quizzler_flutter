6e0ebe02ca59014e85da32dd2c4ab22686d5df71
Abhishek Sakibanda <abhisheksakibanda@gmail.com>
2023-08-30 17:41:32 -0400

    implemented quiz logic and fixed overflow error

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -26,13 +29,6 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.'
-  ];
-
-  int questionNumber = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -46,7 +42,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -67,9 +63,20 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                // The user picked true
+                if (quizBrain.getQuestionAnswer()) {
+                  scoreKeeper.add(Icon(
+                    Icons.check,
+                    color: Colors.green,
+                  ));
+                } else {
+                  scoreKeeper.add(Icon(
+                    Icons.close,
+                    color: Colors.red,
+                  ));
+                }
                 setState(() {
-                  // The user picked true
-                  questionNumber++;
+                  quizBrain.nextQuestion();
                 });
               },
               style: ButtonStyle(
@@ -91,7 +98,20 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 // The user picked false
-                questionNumber++;
+                if (!quizBrain.getQuestionAnswer()) {
+                  scoreKeeper.add(Icon(
+                    Icons.check,
+                    color: Colors.green,
+                  ));
+                } else {
+                  scoreKeeper.add(Icon(
+                    Icons.close,
+                    color: Colors.red,
+                  ));
+                }
+                setState(() {
+                  quizBrain.nextQuestion();
+                });
               },
               style: ButtonStyle(
                 backgroundColor: MaterialStatePropertyAll(Colors.red),
